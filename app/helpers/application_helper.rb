c7d481cda4bef0dadffc1d2b8839358e2aaf78a2
borenho <borecollins@gmail.com>
2016-12-02 05:22:07 +0300

    Added gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def avatar_url(user)  #get avatar using user's email
+		gravatar_id = Digest::MD5::hexdigest(user.email).downcase  #get the email, encode it with MD5 then downcase
+		"https://s.gravatar.com/avatar/08a24f97e85fb123abbee1db8450f0c5?s=80"  #gravatar gotten from grabatar.com
+	end
 end
