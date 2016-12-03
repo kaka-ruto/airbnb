1f4608068d7cbd9af023e8722f71949fe2710c23
borenho <borecollins@gmail.com>
2016-12-03 18:37:19 +0300

    Validated Room Model and created Rooms Controller

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,44 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:show]
+
+  def index
+    @rooms = current_user.rooms
+  end
+
+  def show
+    
+  end
+
+  def new
+    @room = current_user.rooms.build
+  end
+
+  def create
+    @room = current_user.rooms.build
+    if @room.save
+      redirect_to @room, notice: "saved..."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to @room, notice: "Updated..."
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_room
+      @room = Room.find(params[:id])
+    end
+
+    def room_params
+      params.require(:room).permit(:home_type, :room_type, :accomodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, :is_heating, :is_internet, :price, :active)
+end
