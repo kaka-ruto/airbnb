fe919f629165207181953320c8f13063b746bec7
borenho <borecollins@gmail.com>
2016-12-05 14:06:42 +0300

    Debugging the room and listings view

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,3 +1,5 @@
+# 
+
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:show]
@@ -16,15 +18,17 @@ class RoomsController < ApplicationController
 
   def create
     @room = current_user.rooms.build(room_params)
+
     if @room.save
-      if params[:images]
+
+      if params[:images] 
         params[:images].each do |image|
           @room.photos.create(image: image)
         end
       end
 
       @photos = @room.photos
-      redirect_to edit_room_path(@room), notice: "saved..."
+      redirect_to edit_room_path(@room), notice: "Saved..."
     else
       render :new
     end
@@ -34,17 +38,19 @@ class RoomsController < ApplicationController
     if current_user.id == @room.user.id
       @photos = @room.photos
     else
-      redirect_to root_path, notice: "You don't have permission to edit"
+      redirect_to root_path, notice: "You don't have permission."
     end
   end
-
+ 
   def update
     if @room.update(room_params)
-      if params[:images]
+
+      if params[:images] 
         params[:images].each do |image|
           @room.photos.create(image: image)
         end
       end
+
       redirect_to edit_room_path(@room), notice: "Updated..."
     else
       render :edit
@@ -53,10 +59,10 @@ class RoomsController < ApplicationController
 
   private
     def set_room
-      @room = Room.find(params[:id])
+      @room = Room.find(params[:id]) 
     end
 
     def room_params
-      params.require(:room).permit(:home_type, :room_type, :accomodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, :is_heating, :is_internet, :price, :active)
+      params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, :is_heating, :is_internet, :price, :active)
     end
 end
